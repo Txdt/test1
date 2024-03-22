40973f2395ee5ab9f5fdde1a0fb8988f79253257
bing1992 <1520325759@qq.com>
2024-03-22 20:37:37 +0800

    登录验证码第三方识别

---
+++ msjy_loggi.py
@@ -0,0 +1,39 @@
+import json
+
+import requests
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+url = "http://47.107.116.139/fangwei/m.php?m=Public&a=login"
+driver.get(url)
+driver.maximize_window()
+# 对验证码截图
+driver.find_element(By.XPATH, '//*[@id="verify"]').screenshot('verify.png')
+# 第三方识别验证码
+url = 'http://upload.chaojiying.net/Upload/Processing.php'
+data = {
+    "user": "baili123",
+    # 密码：pass原生密码，pass2（md5加密）
+    "pass2": "02BB7F3BBBAC170A2D836517AD9CC8DA",
+    "sofid": "958623", # 软件ID
+    "codetype": 1902
+}
+files = {"userfile": open("verify.png", "rb")}
+resp = requests.post(url=url, data=data, files=files)
+res = resp.json()
+code = ""
+if res["err_no"] == 0:
+    code = res["pic_str"]
+    print(f"识别成功{code}")
+else:
+    print("识别失败")
+
+driver.find_element(By.XPATH, '/html/body/form/table/tbody/tr/td[3]/table/tbody/tr[2]/td[2]/input').send_keys("admin")
+driver.find_element(By.XPATH, '/html/body/form/table/tbody/tr/td[3]/table/tbody/tr[3]/td[2]/input').send_keys("msjy123")
+driver.find_element(By.XPATH, '/html/body/form/table/tbody/tr/td[3]/table/tbody/tr[5]/td[2]/input').send_keys(code)
+driver.find_element(By.XPATH, '//*[@id="login_btn"]').click()
+
+time.sleep(5)
+driver.quit()
